24b23978acd60d28c12e121284483d93bb169bc9
meraj arshad <merajsap184@gmail.com>
2023-08-09 11:30:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let imgarea = document.querySelector(".img");
+let clickCount =document.querySelector(".clickCount");
+
+
+function showHeart(){
+    const heart = document.createElement("i");
+    heart.classList.add("fa-solid");
+    heart.classList.add("fa-heart");
+    imgarea.appendChild(heart);
+
+
+}
+
+imgarea.addEventListener("dblclick",function(){
+    showHeart();
+    counter();
+})
+
+let count =0;
+function counter(){
+    clickCount.innerHTML =++count;
+    
+}
